d1c4243112e5047f4713f0f3834942bedfc701b9
Sheik Hazrin <sheikhazrin@kiswire.com>
2024-05-23 15:53:11 +0800

    publish server 194.1.31.8

--- app/(full-page)/layout.tsx
+++ app/(full-page)/layout.tsx
@@ -7,7 +7,7 @@ interface SimpleLayoutProps {
 }
 
 export const metadata: Metadata = {
-    title: 'ENS APP [FULL]',
+    title: 'Machine Monitoring App [v2]',
     description: 'The ultimate collection of design-agnostic, flexible and accessible React UI Components.'
 };
 
